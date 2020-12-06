90cc24d998420f105ff5e93300dff0d1043eafe2
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2020-12-06 11:38:32 -0800

    Added function to write an html file once all prompts have been entered by the user.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-//Pathways
+//Pathways to output and to output/team.html
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -15,6 +15,7 @@ const render = require("./lib/htmlRenderer");
 
 //List of Employees
 const employees = [];
+
 
 //Function to receive Manager's information.
 function managerInformation() {
@@ -54,6 +55,7 @@ function managerInformation() {
         });
 };
 
+
 //Function to ask if the user would like to add another employee.
 function addEmployee() {
 
@@ -75,7 +77,8 @@ function addEmployee() {
             //If the user does not want to add more employees, will render html file.  Otherwise, employeeInformation function will be called.
             if(response.chosenRole === 'There are no more employees to be added') {
                 //ADD FUNCTION LATER TO RENDER HTML PAGE!!!!!!!!!!!!
-                console.log('Done', employees)
+                let newHtml = render(employees);
+                createHtmlFile(newHtml);
             } else {
                 employeeInformation(response.chosenRole);
             };
@@ -139,25 +142,24 @@ function employeeInformation(specificRole) {
 };
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+//Function to create a team.html file into the output folder.  If the output folder does not exist, it will create one first, then call the function again to write the file.
+function createHtmlFile(htmlData) {
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    if(!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdir(`${OUTPUT_DIR}`, (err) => {
+            if (err) {
+                throw err
+            } else {
+                createHtmlFile(htmlData);
+            }
+        });
+    } else {
+        fs.writeFile(outputPath, htmlData, (err) => {
+            if(err) console.error(`There was an error: ${err}`);
+        });
+    }
 
-//Call
+}
+
+//managerInformation will be called first
 managerInformation();
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
